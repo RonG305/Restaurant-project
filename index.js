f6fb0740cb005d6065ba269a61f35490bdecf3cd
RonG305 <106582548+RonG305@users.noreply.github.com>
2022-12-01 17:28:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const home = document.querySelector(".home")
+home.onclick = () => {
+    window.location.href = "index.html"
+}
+
+const about = document.querySelector(".about-page")
+about.onclick = () => {
+    window.location.href = "about.html"
+}
+
+const services = document.querySelector(".services-page")
+services.onclick = () => {
+    window.location.href = "services.html"
+}
+
+const open = document.getElementById("menu-bar")
+const close = document.getElementById("close-bar")
+const navbar = document.querySelector(".nav_links")
+
+open.onclick = () => {
+    close.style.display = "flex"
+    navbar.style.display = "flex"
+    open.style.display = "none"
+}
+
+close.onclick = () => {
+    close.style.display = "none"
+    navbar.style.display = "none"
+    open.style.display = "flex"
+}
+
